6f23fc9af4dadb813a0f017b1274c0d33deae910
lejabque <vorkdenis1@gmail.com>
2022-10-14 18:54:24 +0300

    use vcpkg

--- function.h
+++ function.h
@@ -1,31 +1,32 @@
 #pragma once
 
+struct bad_function_call {};
+
 template <typename F>
 struct function;
 
 template <typename R, typename... Args>
-struct function<R (Args...)>
-{
-    function() noexcept;
+struct function<R(Args...)> {
+  function() noexcept;
 
-    function(function const& other);
-    function(function&& other) noexcept;
+  function(function const& other);
+  function(function&& other) noexcept;
 
-    template <typename T>
-    function(T val);
+  template <typename T>
+  function(T val);
 
-    function& operator=(function const& rhs);
-    function& operator=(function&& rhs) noexcept;
+  function& operator=(function const& rhs);
+  function& operator=(function&& rhs) noexcept;
 
-    ~function();
+  ~function();
 
-    explicit operator bool() const noexcept;
+  explicit operator bool() const noexcept;
 
-    R operator()(Args... args) const;
+  R operator()(Args... args) const;
 
-    template <typename T>
-    T* target() noexcept;
+  template <typename T>
+  T* target() noexcept;
 
-    template <typename T>
-    T const* target() const noexcept;
+  template <typename T>
+  T const* target() const noexcept;
 };
